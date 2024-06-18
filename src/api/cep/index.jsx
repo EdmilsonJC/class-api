716ec27459d9fdef5157ba9efe971be0140bd540
EdmilsonJC <edcz2902@gmail.com>
2024-06-17 22:15:51 -0300

    feat: create d the api directory  to be api consu

---
+++ src/api/cep/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function GetCep() {
+  const [cep, setCep] = useState("");
+  const [address, setAddress] = useState(null);
+
+  async function handleAddress() {
+    try {
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+
+      setAddress(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>API Busca CEP</h1>
+      <input type="text" onChange={(e) => setCep(e.target.value)} />
+      <button onClick={handleAddress}>Busque seu CEP!</button>
+
+      {address && (
+        <div>
+          <p>Rua: {address.logradouro}</p>
+          <p>DDD: {address.ddd}</p>
+          <p>Bairro: {address.bairro}</p>
+          <p>Cidade: {address.localidade}</p>
+        </div>
+      )}
+    </div>
+  );
+}
